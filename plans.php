2d4dd3dd7a56894a50c3e66577f55b38eabf0c95
pedro moreno <pedroduban15@gmail.com>
2022-07-11 13:35:38 -0500

    Commit 1
    11/07/2022

---
+++ plans.php
@@ -0,0 +1,102 @@
+<section class="pricing-table section">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title">
+                        <h2 class="wow fadeInUp" data-wow-delay=".4s">Pricing Plan</h2>
+                        <p class="wow fadeInUp" data-wow-delay=".6s">There are many variations of passages of Lorem
+                            Ipsum available, but the majority have suffered alteration in some form.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-12">
+
+                    <div class="single-table wow fadeInUp" data-wow-delay=".2s">
+
+                        <div class="table-head">
+                            <div class="price">
+                                <h2 class="amount">$00<span class="duration">/ Month</span></h2>
+                            </div>
+                            <h4 class="title">Free</h4>
+                        </div>
+
+
+                        <ul class="table-list">
+                            <li>One Listing</li>
+                            <li>Contact Display</li>
+                            <li>Image Gallery</li>
+                            <li>30 Days Availablity</li>
+                            <li>Non-Featured</li>
+                            <li>Business Tagline</li>
+                        </ul>
+
+
+                        <div class="button">
+                            <a class="btn" href="javascript:void(0)">Select Plan</a>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+
+                    <div class="single-table wow fadeInUp" data-wow-delay=".4s">
+
+                        <div class="table-head">
+                            <div class="price">
+                                <h2 class="amount">$59<span class="duration">/ Month</span></h2>
+                            </div>
+                            <h4 class="title">Standard</h4>
+                        </div>
+
+
+                        <ul class="table-list">
+                            <li>One Listing</li>
+                            <li>Contact Display</li>
+                            <li>Image Gallery</li>
+                            <li>60 Days Availablity</li>
+                            <li>Non-Featured</li>
+                            <li>Business Tagline</li>
+                        </ul>
+
+
+                        <div class="button">
+                            <a class="btn" href="javascript:void(0)">Select Plan</a>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+
+                    <div class="single-table wow fadeInUp" data-wow-delay=".6s">
+
+                        <div class="table-head">
+                            <div class="price">
+                                <h2 class="amount">$99<span class="duration">/ Month</span></h2>
+                            </div>
+                            <h4 class="title">Premium</h4>
+                        </div>
+
+
+                        <ul class="table-list">
+                            <li>One Listing</li>
+                            <li>Contact Display</li>
+                            <li>Image Gallery</li>
+                            <li>90 Days Availablity</li>
+                            <li>Non-Featured</li>
+                            <li>Business Tagline</li>
+                        </ul>
+
+
+                        <div class="button">
+                            <a class="btn" href="javascript:void(0)">Select Plan</a>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
